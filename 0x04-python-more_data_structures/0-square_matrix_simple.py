f5ea3be8b4ef2cf25f1bbf9ebc6a600a7080cefa
abeersani <aburi.ms@hotmail.com>
2022-12-07 01:41:27 -0800

    third

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    dup_matrix = matrix.copy()
-    for line in range(len(matrix)):
-        dup_matrix[line] = list(map((lambda x: x**2), matrix[line]))
-    return(dup_matrix)
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
